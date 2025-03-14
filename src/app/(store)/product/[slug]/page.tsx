49dd318ee24d6a4da770a542407c0df3eba074db
Cristiane Martins <crisplusm@gmail.com>
2025-03-13 22:19:19 -0300

    Fetch de dados do Produto

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,64 +1,95 @@
+import { api } from "@/data/api";
+import { Product } from "@/data/types/product";
 import Image from "next/image";
 
-export default function ProductPage() {
-  return (
-    <div className="relative grid max-h-[860px] grid-cols-3">
-      <div className="col-span-2 overflow-hidden">
-        <Image 
-          src={"/moletom-never-stop-learning.png"} 
-          alt="Never Stop Learning" 
-          width={1000} 
-          height={1000} 
-          quality={100} />
-      </div>
+interface ProductProps {
+    params: {
+        slug: string;
+    }
+}
 
-      <div className="flex flex-col justify-center px-12 ">
-          <h1 className="text-3xl font-bold leanding-tight ">
-              Moletom Never Stop Learning
-          </h1>
+async function getProduct(slug: string): Promise<Product> {
+    const response = await api(`/products/${slug}`, {
+        next: {
+            revalidate: 25 * 25,
+        }
+    });
 
-          <p className="mt-2 leanding-relaxed text-zinc-400">
-              Moletom fabricado com 88% de algodão e 12% de poliéster.
-          </p>
+    const products = await response.json();
 
-          <div className="mt-8 flex items-center gap-3">
-              <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-                  R$129
-              </span>
-              <span className="text-sm text-zinc-400">
-                Em 12 s/ sem juros de R$10,75
-              </span>
-          </div>
+    return products
+}
 
-          <div className="mt-8 space-y-4">
-              <span className="block font-semibold">Tamanhos</span>
+export default async function ProductPage({ params }: ProductProps) {
+    const product = await getProduct(params.slug)
 
-              <div className="flex gap-2">
-                  <button type="button" className="flex h-9 w-14 items-center justify-center rounded-full border  border-zinc-700 bg-zinc-800 text-sm font-semibold"
-                  >
-                      P
-                  </button>
+    return (
+        <div className="relative grid max-h-[860px] grid-cols-3">
+            <div className="col-span-2 overflow-hidden">
+                <Image
+                    src={product.image}
+                    alt="Never Stop Learning"
+                    width={1000}
+                    height={1000}
+                    quality={100} />
+            </div>
 
-                  <button type="button" className="flex h-9 w-14 items-center justify-center rounded-full border  border-zinc-700 bg-zinc-800 text-sm font-semibold"
-                  >
-                      M
-                  </button>
+            <div className="flex flex-col justify-center px-12 ">
+                <h1 className="text-3xl font-bold leanding-tight ">
+                    {product.title}
+                </h1>
 
-                  <button type="button" className="flex h-9 w-14 items-center justify-center rounded-full border  border-zinc-700 bg-zinc-800 text-sm font-semibold"
-                  >
-                      G
-                  </button>
+                <p className="mt-2 leanding-relaxed text-zinc-400">
+                    {product.description}
+                </p>
 
-                  <button type="button" className="flex h-9 w-14 items-center justify-center rounded-full border  border-zinc-700 bg-zinc-800 text-sm font-semibold"
-                  >
-                      GG
-                  </button>
-              </div>
-          </div>
+                <div className="mt-8 flex items-center gap-3">
+                    <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
+                        {product.price.toLocaleString('pt-BR', {
+                            style: 'currency',
+                            currency: 'BRL',
+                            minimumFractionDigits: 0,
+                            maximumFractionDigits: 0,
+                        })}
+                    </span>
+                    <span className="text-sm text-zinc-400">
+                        {(product.price / 12).toLocaleString('pt-BR', {
+                            style: 'currency',
+                            currency: 'BRL',
 
-          <button type="button" className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white">Adcionar ao carrinho</button>
-      </div>
-    </div>
-    
-  )
+                        })}
+                    </span>
+                </div>
+
+                <div className="mt-8 space-y-4">
+                    <span className="block font-semibold">Tamanhos</span>
+
+                    <div className="flex gap-2">
+                        <button type="button" className="flex h-9 w-14 items-center justify-center rounded-full border  border-zinc-700 bg-zinc-800 text-sm font-semibold"
+                        >
+                            P
+                        </button>
+
+                        <button type="button" className="flex h-9 w-14 items-center justify-center rounded-full border  border-zinc-700 bg-zinc-800 text-sm font-semibold"
+                        >
+                            M
+                        </button>
+
+                        <button type="button" className="flex h-9 w-14 items-center justify-center rounded-full border  border-zinc-700 bg-zinc-800 text-sm font-semibold"
+                        >
+                            G
+                        </button>
+
+                        <button type="button" className="flex h-9 w-14 items-center justify-center rounded-full border  border-zinc-700 bg-zinc-800 text-sm font-semibold"
+                        >
+                            GG
+                        </button>
+                    </div>
+                </div>
+
+                <button type="button" className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white">Adcionar ao carrinho</button>
+            </div>
+        </div>
+
+    )
 }
